f6fe65bbd24db5bd7541025292a588c207413ce1
AliXuB2606 <alixub2006@gmail.com>
2022-05-04 17:33:29 +0500

    main

--- src/components/ForCard.vue
+++ src/components/ForCard.vue
@@ -8,12 +8,58 @@
           </q-img>
         </div>
         <div>
-
-          <q-icon class="q-mr-lg " name="shopping_cart" size="28px" />
+          <select class="q-ma-sm" name="" id="">
+            <option value="">
+              Digital Agency
+            </option>
+          </select>
+          <select class="q-ma-sm" name="" id="">
+            <option value="">
+             Services
+            </option>
+          </select>
+          <select class="q-ma-sm" name="" id="">
+            <option value="">
+              Portfolio
+            </option>
+          </select>
+          <select class="q-ma-sm" name="" id="">
+            <option value="">
+              Pages
+            </option>
+          </select>
+          <select class="q-ma-sm" name="" id="">
+            <option value="">
+              Blog
+            </option>
+          </select>
+          <a class="q-ma-md" href="#">Contact</a>
+          <q-icon class="q-mr-lg icon " name="shopping_cart" size="28px" />
         </div>
 
       </div>
     </nav>
+  </div>
+  <div class="fit row justify-center items-center q-mt-xl bg-grey-5 q-pa-sm ">
+    <footer class=" fit row justify-around items-center">
+        <div style="" >
+          <span class="text-grey-8" >
+            Copyright © 2022 Creative Agency WordPress Theme by <a href="#"> Axilthemes</a> . All rights reserved
+          </span>
+        </div>
+        <div>
+          <a class="q-ma-md" href="#">Terms of Use</a>
+          <a href="#">Privacy Policy</a>
+        </div>
+    </footer>
+
+  </div>
+  <div class="">
+    <nav>
+        <div>
+          <span>XuB</span>
+        </div>
+      </nav>
   </div>
 </template>
 <script>
@@ -42,6 +88,13 @@ export default {
     transition: 0.2s ease;
   }
   a:hover{
+     color: rgb(145, 54, 237);
+    transition: 0.2s ease;
+  }
+  .icon{
+    cursor: pointer;
+  }
+   .icon:hover{
      color: rgb(145, 54, 237);
     transition: 0.2s ease;
   }
